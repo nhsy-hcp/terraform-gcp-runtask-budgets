95e5ad433045f82bc59b25ff85968ddadf072750
nhsy-hcp <126588262+nhsy-hcp@users.noreply.github.com>
2023-07-25 11:30:35 +0100

    Added terraform pytest

---
+++ tests/test_default.py
@@ -0,0 +1,98 @@
+import json
+import os
+import pytest
+import requests
+import tftest
+
+BASE_DIR = f"{os.path.dirname(__file__)}/../terraform"
+
+
+@pytest.fixture(scope="session")
+def plan():
+    tf = tftest.TerraformTest(tfdir=BASE_DIR)
+    tf.setup(use_cache=True)
+    return tf.plan(output=True, use_cache=True)
+
+
+@pytest.fixture(scope="session")
+def apply():
+    targets = []
+    # targets = [
+    #     "google_storage_bucket.cloud_functions",
+    #     "google_cloudfunctions2_function.runtask_process"
+    # ]
+    tf = tftest.TerraformTest(BASE_DIR)
+    tf.setup(use_cache=True, cleanup_on_exit=True)
+    tf.apply(output=True, use_cache=True, targets=targets)
+    yield tf.output()
+    tf.destroy(use_cache=True, **{"auto_approve": True})
+
+
+@pytest.fixture(scope="session")
+def state(apply):
+    with open(os.path.join(BASE_DIR, "terraform.tfstate")) as state_file:
+        return tftest.TerraformState(json.load(state_file))
+
+
+@pytest.mark.plan
+def test_variables(plan):
+    tf_vars = plan.variables
+    assert "project_id" in tf_vars, "project_id not in tf var plan"
+
+
+@pytest.mark.plan
+def test_plan_gcs_cloud_function(plan):
+    res = plan.resources['google_storage_bucket.cloud_functions']
+    assert 'google_storage_bucket.cloud_functions' in plan.resources
+    assert res['values']['location'] == plan.variables['region'].upper()
+    assert res['values']['uniform_bucket_level_access'] == True
+
+
+def test_state_gcs_cloud_function(state):
+    id = state.resources["None.random_string.suffix"]['instances'][0]['attributes']['id']
+    gcs_id = state.resources["None.google_storage_bucket.cloud_functions"]['instances'][0]['attributes']['id']
+    assert gcs_id == "runtask-cloud-functions-{}".format(id)
+
+
+def test_state_cloud_function_callback(state):
+    cloud_function_state = state.resources["None.google_cloudfunctions2_function.runtask_callback"]['instances'][0]['attributes']['state']
+    assert cloud_function_state == "ACTIVE"
+
+
+def test_state_cloud_function_process(state):
+    cloud_function_state = state.resources["None.google_cloudfunctions2_function.runtask_process"]['instances'][0]['attributes']['state']
+    assert cloud_function_state == "ACTIVE"
+
+
+def test_state_cloud_function_request(state):
+    cloud_function_state = state.resources["None.google_cloudfunctions2_function.runtask_request"]['instances'][0]['attributes']['state']
+    assert cloud_function_state == "ACTIVE"
+
+
+def test_apply_outputs(apply):
+    assert len(apply) > 0
+
+
+def test_apply_cloud_function_callback_uri(apply):
+    url = apply["runtask_callback_uri"]
+
+    with pytest.raises(requests.exceptions.HTTPError):
+        with requests.get(url) as r:
+            r.raise_for_status()
+
+
+def test_apply_cloud_function_request_uri(apply):
+    url = apply["runtask_request_uri"]
+
+    with requests.get(url) as r:
+        r.raise_for_status()
+
+    assert r.status_code == 200
+
+
+def test_apply_cloud_function_process_uri(apply):
+    url = apply["runtask_process_uri"]
+
+    with pytest.raises(requests.exceptions.HTTPError):
+        with requests.get(url) as r:
+            r.raise_for_status()
