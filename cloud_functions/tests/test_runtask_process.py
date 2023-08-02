e486dad41663b8f76cb21298632ec871e50adf11
nhsy-hcp <126588262+nhsy-hcp@users.noreply.github.com>
2023-08-02 17:40:07 +0100

    Updated pytests

--- cloud_functions/tests/test_runtask_process.py
+++ cloud_functions/tests/test_runtask_process.py
@@ -1,14 +1,39 @@
 import os
 import sys
+
 sys.path.insert(0, f"{os.path.dirname(__file__)}/../runtask_process")
 
 from unittest.mock import Mock
+import pytest
 from runtask_process import main
+from runtask_process import googleproject
+
+
+@pytest.fixture(scope="session")
+def proj() -> googleproject.GoogleProject:
+    proj = googleproject.GoogleProject()
+    proj.get(proj.default_project_id)
+
+    return proj
 
 
 def test_process_handler():
     data = {
-        "workspace_name": "00000"
+        "access_token": "00000",
+        "plan_json_api_url": "00000",
     }
     req = Mock(get_json=Mock(return_value=data), args=data)
-    assert main.process_handler(req) == ({'message': 'Google Cloud Runtask Budgets - Google project label lookup failed', 'status': 'failed'}, 200)
+    assert main.process_handler(req) == (
+        {
+            "message": "Google Cloud Runtask Budgets - TFC plan download or parse failed",
+            "status": "failed",
+        },
+        200,
+    )
+
+
+def test_validate_projects_ids(proj):
+    validate_result, validate_msg = main.__validate_project_ids(
+        [proj.project.project_id]
+    )
+    assert validate_result in [True, False]
