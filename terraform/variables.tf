85a2798eb44bbd8ec413d2d37be67ff06478bb0c
nhsy-hcp <126588262+nhsy-hcp@users.noreply.github.com>
2023-08-02 12:33:10 +0100

    Updated terraform variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,13 +1,18 @@
-variable "project_id" {}
+variable "project_id" {
+  type = string
+}
 
 variable "region" {
+  type    = string
   default = "europe-west1"
 }
 
 variable "hmac_key" {
+  type    = string
   default = "secret"
 }
 
 variable "project_viewer" {
-  type = string
+  type    = string
+  default = null
 }
